29b1c2b1276e2e9ed67fc46b6f685ff0d2a80f84
kiariepeter <47138682+kiariepeter@users.noreply.github.com>
2019-02-08 13:16:52 +0300

    added a base route

--- run.py
+++ run.py
@@ -3,6 +3,11 @@ from app.api.app import create_app
 
 app = create_app()
 
+@app.route('/')
+def hello_world():
+    return 'welcome to ploiticov1'
 
 if __name__ == '__main__':
     app.run(debug= True)
+
+
