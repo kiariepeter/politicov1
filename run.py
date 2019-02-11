4ab7c23026fd86af34b2acf4c8b7b18217995ee9
pkiariepope <pope2peter4@gmail.com>
2019-02-11 21:05:41 +0300

    removed spaces

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-
+import os
 from app import create_app
 
 app = create_app()
@@ -6,7 +6,7 @@ app = create_app()
 
 @app.route('/')
 def hello_world():
-    return 'welcome to ploiticov1'
+    return 'Hello, World!'
 
 
 if __name__ == '__main__':
