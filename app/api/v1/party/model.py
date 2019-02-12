f2cf9b872ee7b95995a84d55ffd2e8e716fbcecb
pkiariepope <pope2peter4@gmail.com>
2019-02-12 21:13:47 +0300

    updated the party model data structure from list to dictionaries

--- app/api/v1/party/model.py
+++ app/api/v1/party/model.py
@@ -1,48 +1,46 @@
 from flask import make_response, jsonify, request
 
-parties = []
+parties = {}
 
 
 class Party(object):
-    def __init__(self, party_name, logo, members):
-        self.party_id = len(parties) + 1
-        self.party_name = party_name
-        self.logo = logo
-        self.members = members
-        self.parties = parties.append(
-            {'party_id': 1, 'party_name': 'Nark', 'logo': 'http://www.lpolitio.com/logo.jpg', 'members': 40})
-
-    def add_party(self):
-        """This method saves party data"""
-        new_party = {
-            "party_id": len(parties) + 1,
-            "party_name": self.party_name,
-            "logo": self.logo,
-            "members": self.members
-        }
-        parties.append(new_party)
-
-    def get_parties(self):
-        """This method shows all political parties in the list"""
-        return self.parties
+    def __init__(self):
+        self.party_id = 0
+        self.party_name = ''
+        self.logo = ''
+        self.members = ''
+        self.parties = parties
 
     @staticmethod
-    def edit_party(party_id):
-        """This method edits  a specific political party in the list"""
-        task = [party for party in parties if party["party_id"] == party_id]
-        if not task:
-            return make_response(jsonify({
-                "status": "OK",
-                "Message": "party not found"
-            }), 404)
-        task[0]['members'] = request.json.get('members', task[0]['members'])
-        task[0]['logo'] = request.json.get('logo', task[0]['logo'])
-        task[0]['party_name'] = request.json.get('party_name', task[0]['party_name'])
+    def add_party(party_name, logo, members):
+        """This method saves party data"""
+        size = len(parties) + 1
+        new_party = {
+            "party_id": size,
+            "party_name": party_name,
+            "logo": logo,
+            "members": members
+        }
+        parties[size] = new_party
+
+    def get_parties(self):
+        """This method shows all political parties in the dictionary"""
+        return self.parties
+
+    def edit_party(self, party_id, party_name, logo, members):
+        """This method edits  a specific political party in the dictionary"""
+        if party_id:
+            if party_id in self.parties:
+                parties[party_id]['party_name'] = party_name
+                parties[party_id]['logo'] = logo
+                parties[party_id]['members'] = members
+
+                return make_response(jsonify({
+                    "status": 201,
+                    "message": "Party Updated successfully",
+                    "new details": self.parties
+                }), 201)
         return make_response(jsonify({
-            "status": 201,
-            "message": "Party Updated successfully",
-            "new details": task[0]
-        }), 201)
-
-
-
+            "status": 404,
+            "Message": "party not found"
+        }), 404)
