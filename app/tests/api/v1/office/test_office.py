999ad74d00d0f2a07be1e63cc3b4d985d386bf1a
pkiariepope <pope2peter4@gmail.com>
2019-02-12 21:38:35 +0300

    updated entire code base to use lists

--- app/tests/api/v1/office/test_office.py
+++ app/tests/api/v1/office/test_office.py
@@ -8,20 +8,20 @@ class OfficeTests(OfficeTest):
 
     def test_office_party(self):
         """Test API can create a office"""
-        response = self.client().post('/api/v1/add_office', data=self.add_office, content_type='application/json')
+        response = self.client().post('/api/v1/offices', data=self.add_office, content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
     def test_get_all_office(self):
         """Tests API can get all offices"""
-        response = self.client().get('/api/v1/get_offices')
+        response = self.client().get('/api/v1/offices')
         self.assertEqual(response.status_code, 201)
 
-    def test_get_a_specific_office_by_id(self):
+    def test_get_a_specific_office(self):
         """Tests API can get a specific office by using its id"""
-        create_response = self.client().post('/api/v1/add_office', data=self.add_office,
+        create_response = self.client().post('/api/v1/offices', data=self.add_office,
                                              content_type='application/json')
         if create_response:
-            response = self.client().get('/api/v1/get_offices')
+            response = self.client().get('/api/v1/offices/1')
             self.assertEqual(response.status_code, 201)
 
 
