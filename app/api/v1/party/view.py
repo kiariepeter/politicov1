df695a1bc92941854df79a6f3611cb5dcd1c3f1b
pkiariepope <pope2peter4@gmail.com>
2019-02-12 21:19:49 +0300

    [ft-update-datastructure #163915637] updated the party view  data structure from list to dictionaries

--- app/api/v1/party/view.py
+++ app/api/v1/party/view.py
@@ -4,17 +4,17 @@ from app.api.v1.party.model import Party, parties
 party_Blueprint = Blueprint('party', __name__)
 
 
-@party_Blueprint.route('/get_parties')
+@party_Blueprint.route('/parties')
 def get_parties():
     """Given that i am an admin i should be able to get a list of all political parties
-       When i visit .../api/v1/get_parties endpoint using GET method"""
+       When i visit .../api/v1/parties endpoint using GET method"""
     return make_response(jsonify(parties), 201)
 
 
-@party_Blueprint.route('/add_party', methods=['POST'])
+@party_Blueprint.route('/parties', methods=['POST'])
 def add_party():
     """Given that i am an admin i should be able to add a political party
-       When i visit .../api/v1/add_party endpoint using POST method """
+       When i visit .../api/v1/parties endpoint using POST method """
     try:
         if not request.get_json():
             return make_response(jsonify({'status': 401}), 401)
@@ -22,8 +22,8 @@ def add_party():
         party_name = party_data['party_name']
         logo = party_data['logo']
         members = party_data['members']
-        party = Party(party_name, logo, members)
-        party.add_party()
+        party = Party()
+        party.add_party(party_name, logo, members)
         return make_response(jsonify({
             "status": 201,
             "message": "Party added successfully",
@@ -35,26 +35,21 @@ def add_party():
         return make_response(jsonify({'message': 'Bad request', 'status': 400}), 400)
 
 
-@party_Blueprint.route('/get_party/<int:party_id>', methods=['GET'])
+@party_Blueprint.route('/parties/<int:party_id>', methods=['GET'])
 def get_party(party_id):
     """Given that i am an admin i should be able to get a  specific political party
-       When i visit .../api/v1/get_party/1 endpoint using GET method"""
+       When i visit .../api/v1/parties/1 endpoint using GET method"""
     if party_id:
-        for party_dict in parties:
-            for key in party_dict:
-                if party_dict[key] == party_id:
-                    return make_response(jsonify({'status': 201, 'party_data': party_dict}), 201)
-                else:
-                    return make_response(jsonify({'status': 404, 'message': 'party not found'}), 404)
-
-    else:
-        return make_response(jsonify({'status': 401, 'message': 'party_id missing'}, 401))
+        if party_id in parties:
+            return make_response(jsonify({'status': 201, 'party': parties.get(party_id)}),201)
+        return make_response(jsonify({'status': 404, 'message': 'party not found'}), 404)
+    return make_response(jsonify({'status': 401, 'message': 'party_id missing'}, 401))
 
 
-@party_Blueprint.route('/update_party/<int:party_id>', methods=['PATCH'])
+@party_Blueprint.route('/parties/<int:party_id>', methods=['PATCH'])
 def update_party(party_id):
     """Given that i am an admin i should be able to edit a specific political party
-       When i visit to .../api/v1/edit_party endpoint using PUT method"""
+       When i visit to .../api/v1/parties endpoint using PUT method"""
     if request.method == "PATCH":
         if party_id:
             if not request.get_json():
@@ -63,32 +58,28 @@ def update_party(party_id):
             party_name = party_data['party_name']
             logo = party_data['logo']
             members = party_data['members']
-            party = Party(party_name, logo, members)
-            res = party.edit_party(party_id)
+            party = Party()
+            res = party.edit_party(party_id,party_name, logo, members)
             return res
         else:
             return make_response(jsonify({'status': 401, 'message': 'party_id missing'}, 401))
 
 
-@party_Blueprint.route('/delete_party/<int:party_id>', methods=['DELETE'])
+@party_Blueprint.route('/parties/<int:party_id>', methods=['DELETE'])
 def delete_party(party_id):
     """Given that i am an admin i should be able to delete a specific political party
-       When i append party_id to .../api/v1/delete_party endpoint using DELETE method"""
+       When i append party_id to .../api/v1/parties endpoint using DELETE method"""
     if request.method == "DELETE":
         if party_id:
-            data = [parties for party in parties if party["party_id"] == party_id]
-            if not data:
+            if party_id in parties:
+                del parties[party_id]
                 return make_response(jsonify({
                     "status": "OK",
-                    "Product": "Political party not found"
-                }), 404)
+                    "Message": "Party deleted deleted successfully",
+                    'parties': parties
+                }), 200)
+            return make_response(jsonify({'status': 404, 'message': 'party not found'}), 404)
 
-            parties.remove(parties[0])
-            return make_response(jsonify({
-                "status": "OK",
-                "Message": "Party deleted deleted successfully"
-            }), 200)
-        else:
-            return make_response(jsonify({'status': 401, 'message': 'party_id missing'}, 401))
-    else:
-        return make_response(jsonify({'message': 'Bad request', 'status': 400}), 400)
+        return make_response(jsonify({'status': 401, 'message': 'party_id missing'}, 401))
+
+    return make_response(jsonify({'message': 'Bad request', 'status': 400}), 400)
