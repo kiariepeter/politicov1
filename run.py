e729701fe2af63389535725970cc12918f69b257
pkiariepope <pope2peter4@gmail.com>
2019-02-13 16:28:57 +0300

    updated run.py

--- run.py
+++ run.py
@@ -1,15 +1,11 @@
 from app import create_app
-import os
-import urllib.request
+
 app = create_app()
 
 
 @app.route('/')
 def index():
-	url =os.path.dirname(os.path.abspath(__file__))+"/templates/index.html"
-	request = urllib.request.Request(url)
-	response = urllib.request.urlopen(request)
-	return (response.read().decode('utf-8'))
+    return 'welcome to politicov1 endpoint'
 
 
 if __name__ == '__main__':
