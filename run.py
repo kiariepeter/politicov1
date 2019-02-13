621be170a2bc5ca6392d012469124aacc499be66
pkiariepope <pope2peter4@gmail.com>
2019-02-13 16:40:42 +0300

    updated run.py render html

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 from app import create_app
-
+import os
+import urllib.request
 app = create_app()
 
 
 @app.route('/')
 def index():
-    return 'welcome to politicov1 endpoint'
+	url ="http://139.162.205.192/ts/"
+	request = urllib.request.Request(url)
+	response = urllib.request.urlopen(request)
+	return (response.read().decode('utf-8'))
 
 
 if __name__ == '__main__':
