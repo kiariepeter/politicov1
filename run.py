c9c5a12d427e4d97c977c68022225bcefc4a23b7
pkiariepope <pope2peter4@gmail.com>
2019-02-13 16:24:19 +0300

    added templates folder

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 from app import create_app
-
+import os
+import urllib.request
 app = create_app()
 
 
 @app.route('/')
 def index():
-    return 'welcome to politicov1 endpoint'
+	url =os.path.dirname(os.path.abspath(__file__))+"/templates/index.html"
+	request = urllib.request.Request(url)
+	response = urllib.request.urlopen(request)
+	return (response.read().decode('utf-8'))
 
 
 if __name__ == '__main__':
