97d24ae2e4947f7da4a3566f53b288279a7c4c20
pkiariepope <pope2peter4@gmail.com>
2019-02-16 08:47:43 +0300

    updated database connection

--- connection.py
+++ connection.py
@@ -1,13 +1,13 @@
 import psycopg2
 
- 
 def connect():
     """ Connect to the PostgreSQL database server """
-    conn = None
     try:
-        # read connection parameters
-        conn = psycopg2.connect(host="localhost",database="ch2", user="root", password="root")
-        conn.autocommit = True
-    except (Exception, psycopg2.DatabaseError) as error:
-        # print(error)
+    	conn = None
+    	#parsing the required database connection inputs
+    	conn = psycopg2.connect(host='localhost', database='ch2', user='root', password='root')
+    	conn.autocommit = True
+    except Exception as e:
+    	print(e)
     return conn
+
